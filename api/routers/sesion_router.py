c1ff3a3ef5b70c916cc1f81d6e4432687b11b152
EdgarLuna <eluna@urchinsys.com>
2022-07-14 10:57:43 -0500

    diferenciando entre verify y refresh en la sesion

--- api/routers/sesion_router.py
+++ api/routers/sesion_router.py
@@ -12,7 +12,7 @@ sesion_router = APIRouter(prefix="/sesion", tags=["Sesion"])
 logger = initiate_logger("sesion")
 
 
-@sesion_router.post("/")
+@sesion_router.post("/refresh")
 async def sesion(token: str = Depends(oauth2_scheme), db: Database = Depends(get_db)):
     '''Verifica la validez del token de sesion contra la Base de Datos'''
     collection: Collection = db.get_collection('AccesoUsuario')
@@ -24,3 +24,16 @@ async def sesion(token: str = Depends(oauth2_scheme), db: Database = Depends(get
     verify_token_payload(token_payload, TokenEnum.LOGIN, auth_user_dict_after_model)
     token = generate_token(delete_from_dict(auth_user_dict_after_model, ['password']))
     return {"access_token": token, "token_type": "bearer"}
+
+
+@sesion_router.post("/verify")
+async def sesion(token: str = Depends(oauth2_scheme), db: Database = Depends(get_db)):
+    '''Verifica la validez del token de sesion contra la Base de Datos'''
+    collection: Collection = db.get_collection('AccesoUsuario')
+    token_payload = decode_token(token)
+    token_email = token_payload.get('email')
+    user_dict_raw = verify_if_user_exists(collection, token_email, 'log_in')
+    auth_user_model = AuthUserModel(**user_dict_raw)
+    auth_user_dict_after_model = jsonable_encoder(auth_user_model)
+    verify_token_payload(token_payload, TokenEnum.LOGIN, auth_user_dict_after_model)
+    return {"msg": "Token valido"}
